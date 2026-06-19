f48104c375a05d1e2679ec9f73ddf39b2f98a1c1
Vanshika Rajput <vanshikaacodes@gmail.com>
2026-06-19 23:33:33 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let imgBox = document.getElementById("imgBox");
+let qrImage = document.getElementById("qrImage");
+let qrText = document.getElementById("qrText");
+
+function generateQR() {
+    if (qrText.value.length > 0) {
+        qrImage.src =
+            "https://api.qrserver.com/v1/create-qr-code/?size=200x200&data="
+            + encodeURIComponent(qrText.value);
+    } else {
+        alert("Please enter text or URL");
+    }
+}
